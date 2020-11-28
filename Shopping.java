39267fba5a00236f3f8081afce32a059484b1677
細田敦史 <hosodaaaaa>
2020-11-28 17:32:20 +0900

    :recycle: 商品購入メニュー内の処理をリファクタリング
    選択された個数分配列ないの商品数を減産する処理を追加

--- Shopping.java
+++ Shopping.java
@@ -7,9 +7,9 @@ public class Shopping {
     Scanner scan = new Scanner(System.in);
     List<Goods> stockList = new ArrayList<Goods>();
 
-    while(true) {
+    while (true) {
       // メニュー表示
-      String[] menus = {"1.商品を追加する", "2.商品一覧を確認する", "3.商品を購入する", "9.終了"};
+      String[] menus = { "1.商品を追加する", "2.商品一覧を確認する", "3.商品を購入する", "9.終了" };
       for (String menu : menus) {
         System.out.println(menu);
       }
@@ -18,7 +18,7 @@ public class Shopping {
       scan.nextLine();
 
       // 各メニューの操作
-      if(selectMenu == 1) {
+      if (selectMenu == 1) {
         int nth = 0;
         while (true) {
           // nth回目の番号を割り振る
@@ -42,7 +42,7 @@ public class Shopping {
           // 判定を取得する
           System.out.print("続けて商品を追加しますか? (y/n): ");
           String nextStatus = scan.nextLine();
-          if(nextStatus.equals("n")) {
+          if (nextStatus.equals("n")) {
             break;
           }
         }
@@ -61,19 +61,35 @@ public class Shopping {
         System.out.print("購入数を選択してください: ");
         int stockBuy = scan.nextInt();
 
+        // 在庫から選択された商品の数を減らして
+        // 番号と一致する配列を上書きする
         int i = 1;
         for (Goods stocks : stockList) {
           if (stockNo == i) {
-            stockList.set(stocks.getItemNum() - stockBuy);
+            Goods setItems = new Goods(stocks.getItemName(), stocks.getItemNum() - stockBuy);
+            stockList.set((stockNo - 1), setItems);
           }
           i++;
         }
 
+        // 更新後の配列を再度表示する
         stocks(stockList);
+
+        System.out.print("続けて商品を購入しますか? (y/n): ");
+        System.out.print(System.in);
+        String nextStatus = scan.nextLine();
+        if (nextStatus.equals("no")) {
+          System.out.println("ご利用ありがとうございました。");
+          break;
+        }
+      } else if (selectMenu == 9) {
+        System.out.println("またのご来店をお待ちしております。");
+        break;
       }
     }
   }
 
+  // 在庫一覧を表示するメソッド
   public static void stocks(List<Goods> stockItems) {
     System.out.println("在庫一覧を表示する");
 
